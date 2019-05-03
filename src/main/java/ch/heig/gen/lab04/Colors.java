c4b2444cf79fd74000a703e719aab01218bc1ea6
Nemanja Pantic <nemo.pantic@gmail.com>
2019-05-03 11:50:09 +0200

    closes #10 Refactor and add Colors

---
+++ src/main/java/ch/heig/gen/lab04/Colors.java
@@ -0,0 +1,22 @@
+package ch.heig.gen.lab04;
+
+public enum Colors {
+    blue(1),red(2),yellow(3);
+
+    private int value;
+
+    Colors(int value){
+        this.value = value;
+    }
+
+    public static Colors withId(int id) {
+
+        for (Colors colors : values()) {
+            if (colors.value == id) {
+                return colors;
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid id for Size.");
+    }
+}
